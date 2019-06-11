8d0177c4ffe181a467e6aa1cbf38ce4437bc39f8
xiaogeleo <1269286430@qq.com>
2019-06-12 00:33:47 +0800

    commit

--- src/pages/index/home.vue
+++ src/pages/index/home.vue
@@ -1,28 +1,68 @@
 <template>
 	<view>
-		<button type="primary" @tap="getLocation">http请求</button>
+    <!-- 轮播 -->
+		<view class="uni-padding-wrap">
+			<view class="page-section swiper">
+				<view class="page-section-spacing">
+					<swiper
+						class="swiper"
+						:indicator-dots="indicatorDots"
+						:autoplay="autoplay"
+						:interval="interval"
+						:duration="duration"
+					>
+						<swiper-item>
+							<view class="swiper-item">A</view>
+						</swiper-item>
+					</swiper>
+				</view>
+			</view>
+		</view>
+    <!-- 滚动公告 -->
+    <view class="uni-swiper-msg">
+			<view class="uni-swiper-msg-icon">
+				<image src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png" mode="widthFix"></image>
+			</view>
+			<swiper vertical="true" autoplay="true" circular="true" interval="3000">
+				<swiper-item v-for="(item, index) in msg" :key="index">
+					<navigator>{{item}}</navigator>
+				</swiper-item>
+			</swiper>
+	  </view>
 	</view>
 </template>
 <script>
-	export default {
-		data() {
-			return {
+export default {
+	data() {
+		return {
+			indicatorDots: true,
+			autoplay: true,
+			interval: 2000,
+      duration: 500,
+      
 
-			}
-		},
-		methods: {
-			getLocation: function () {
-				uni.request({
-					url: '/rest.ashx', 
-					success: (res) => {
-						
-					}
-				});
-			}
+      swiper:[{
+        images: ''
+      }],
+      msg : [
+        '恭喜 小明同学 以 ￥0.86 拍得美的空调...',
+        '恭喜 xtihi 以 ￥4.8 拍得大疆无人机...',
+				'恭喜 小明同学 以 ￥0.86 拍得美的空调...',
+				'恭喜 xtihi 以 ￥4.8 拍得大疆无人机...',
+				'恭喜 小明同学 以 ￥0.86 拍得美的空调...'
+			]
+		};
+	},
+	methods: {
+		getLocation: function() {
+			uni.request({
+				url: "/rest.ashx",
+				success: res => {}
+			});
 		}
 	}
+};
 </script>
 
 <style>
-	
 </style>
--- src/pages/index/ucenter.vue
+++ src/pages/index/ucenter.vue
@@ -13,31 +13,6 @@
 					<!-- #endif -->
 				</view>
 			</view>
-			<view class="userInfo-mobile">
-				<text>17688734674</text>
-			</view>
-		</view>
-		<view class="cu-list menu shadow-lg">
-			<view class="cu-item arrow" @click="VIPCard">
-				<view class="content">
-					<image src="http://r.handnear.com/wx_miniProgram/image/icon/icon_vip.png" class="png" mode="aspectFit"></image>
-					<text class="text-grey">我的会员卡</text>
-				</view>
-				<view class="item-text">1张</view>
-			</view>
-			<view class="cu-item arrow" @click="historyOrder">
-				<view class="content">
-					<image src="http://r.handnear.com/wx_miniProgram/image/icon/icon_order.png" class="png" mode="aspectFit"></image>
-					<text class="text-grey">自助历史订单</text>
-				</view>
-				<view class="item-text">离场后生成</view>
-			</view>
-			<view class="cu-item arrow" @click="shopOrder">
-				<view class="content">
-					<image src="http://r.handnear.com/wx_miniProgram/image/icon/icon_shopping.png" class="png" mode="aspectFit"></image>
-					<text class="text-grey">商城订单</text>
-				</view>
-			</view>
 		</view>
 	</view>
 </template>
@@ -57,7 +32,7 @@
 				id : '0'
 			}
 			request(GoodsList, data).then(function(res) {
-				alert("11")
+				console.log(res)
 			})
 		},
 		methods: {
@@ -89,7 +64,7 @@
 		width: 180rpx;
 		height: 180rpx;
 		border-radius: 50%;
-		border: 4rpx solid #76c9cf;
+		border: 4upx solid #fe7f00;
 		overflow: hidden;
 	}
 
@@ -105,8 +80,10 @@
 	}
 
 	.userInfo-txt-name {
-		font-size: 34rpx;
-		margin-bottom: 10rpx;
+		font-size: 30upx;
+    color: #fff;
+    margin-top: 20urp;
+		margin-bottom: 20upx;
 	}
 
 	.userInfo-mobile {
@@ -123,141 +100,8 @@
 		text-align: center;
 		border-radius: 3rpx;
 	}
-
-	.vipPayBox {
-		width: 559rpx;
-		height: 430rpx;
-		position: absolute;
-		top: 2%;
-		left: 50%;
-		transform: translateX(-50%);
-		background-color: #fff;
-		border-radius: 8rpx;
-	}
-
-	.vipPayBox-title {
-		line-height: 96rpx;
-		height: 96rpx;
-		text-align: center;
-		position: relative;
-		margin-top: 24rpx;
-		font-size: 36rpx;
-		color: #000
-	}
-
-	.vipPayBox-main {
-		height: 209rpx;
-		width: 100%;
-		position: relative;
-	}
-
-	.vipCardId {
-		width: 100%;
-		text-align: center;
-		font-size: 32rpx;
-		color: #000;
-		position: absolute;
-		top: 28rpx;
-		left: 0rpx;
-	}
-
-	.shouldPayMoney {
-		width: 100%;
-		text-align: center;
-		font-size: 60rpx;
-		color: #000;
-		position: absolute;
-		top: 91rpx;
-		left: 0rpx;
-	}
-
-	.vipPayBox-ipt {
-		width: 481rpx;
-		height: 80rpx;
-		line-height: 80rpx;
-		background-color: #f2f5f5;
-		font-size: 30rpx;
-		color: #262626;
-		margin-left: 30rpx;
-		padding-left: 19rpx;
-	}
-
-	.vipPayBox-authCodeBox {
-		position: relative;
-	}
-
-	.vipPayBox-authCode {
-		width: 370rpx;
-		height: 80rpx;
-		line-height: 80rpx;
-		background-color: #f2f5f5;
-		font-size: 30rpx;
-		color: #262626;
-		padding-left: 19rpx;
-		position: absolute;
-		left: 30rpx;
-		top: 20rpx;
-	}
-
-	.timer {
-		position: absolute;
-		left: 401rpx;
-		top: 20rpx;
-		width: 110rpx;
-		height: 61rpx;
-		background-color: #e0fdff;
-		border-radius: 0 8rpx 8rpx 0;
-		color: gray;
-		font-size: 30rpx;
-		text-align: center;
-		padding-top: 20rpx;
-		padding-left: 20rpx;
-	}
-
-	.vipPayBox-authCodeBtn {
-		position: absolute;
-		left: 401rpx;
-		top: 20rpx;
-		width: 110rpx;
-		height: 61rpx;
-		background-color: #e0fdff;
-		border-radius: 0 8rpx 8rpx 0;
-		color: #37bfc7;
-		font-size: 30rpx;
-		text-align: center;
-		padding-top: 20rpx;
-		padding-left: 20rpx;
-	}
-
-	.vipPayBox-checkCard {
-		width: 50%;
-		height: 100rpx;
-		line-height: 100rpx;
-		font-size: 36rpx;
-		background-color: #37bfc8;
-		color: #fefefe;
-		position: absolute;
-		bottom: 0rpx;
-		right: 0rpx;
-		border-radius: 0 0 8rpx 0rpx;
-	}
-
-	.vipPayBox-cancel {
-		width: 50%;
-		height: 100rpx;
-		line-height: 100rpx;
-		font-size: 36rpx;
-		background-color: #fff;
-		color: #000000;
-		position: absolute;
-		bottom: 0rpx;
-		left: 0 rpx;
-		border-top: 1rpx solid #e0e0e0;
-		border-radius: 0 0 0rpx 8rpx;
-	}
-
 	.UCenter-bg {
-		height: 500rpx;
+		height: 280rpx;
 		display: flex;
 		justify-content: center;
 		padding-top: 120rpx;
@@ -268,8 +112,8 @@
 		color: #fff;
 		font-weight: 500;
 		text-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
-		background: -webkit-linear-gradient(-180deg, #37bfc8, #149ea8);
-		background: linear-gradient(-180deg, #37bfc8, #149ea8);
+		background: -webkit-linear-gradient(-180deg, #fe7f00, #fe7f00);
+		background: linear-gradient(-180deg, #fe7f00, #fe7f00);
 	}
 
 	.UCenter-bg text {
@@ -289,40 +133,5 @@
 		z-index: 99;
 		mix-blend-mode: screen;
 		height: 100rpx;
-	}
-
-	map,
-	.mapBox {
-		left: 0;
-		z-index: 99;
-		mix-blend-mode: screen;
-		height: 100rpx;
-	}
-
-	map,
-	.mapBox {
-		width: 750rpx;
-		height: 300rpx;
-	}
-
-	.text-xl {
-		margin-top: 20rpx;
-	}
-
-	.cu-list{
-		background-color: #fff;
-	}
-
-	.cu-item{
-		padding: 0;
-	}
-
-	.lines-grey{
-		font-size: 32rpx;
-	}
-
-	.item-text{
-		font-size: 30rpx;
-		color: #a8a8a8;
 	}
 </style>
